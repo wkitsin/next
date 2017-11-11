f06482ca9a758e6f5376db20db41f3531a734f1c
Koo Yi Zen <kooyizen@hotmail.com>
2017-11-11 19:09:36 +0800

    access images through images subfolders

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
-  Rails.application.config.assets.paths << path
-end
-
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
